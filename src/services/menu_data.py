56fb355ee449468cad0ffec473960fa1e3ebb4dd
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-10-16 23:04:13 -0300

    Finalizado requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
-# Req 3
+from models.ingredient import Ingredient
+from models.dish import Dish
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.set_formatted_dishes(source_path)
+
+    def set_formatted_dishes(self, source_path):
+        formatted_dishes = {}
+        with open(source_path, "r") as file:
+            data = csv.DictReader(file)
+
+            for line in data:
+                dish_name = line["dish"]
+                price = float(line["price"])
+                ingredient = Ingredient(line["ingredient"])
+                amount = int(line["recipe_amount"])
+
+                if dish_name not in formatted_dishes:
+                    formatted_dishes[dish_name] = Dish(dish_name, price)
+
+                formatted_dishes[dish_name].add_ingredient_dependency(
+                    ingredient,
+                    amount
+                )
+
+        self.dishes = formatted_dishes.values()
