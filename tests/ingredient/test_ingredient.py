d7a19c85cc0b8c25dbfd80d207ba1e66f5ea3825
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-10-16 21:01:22 -0300

    Finalizado requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,33 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+
+restrictions_manteiga = {
+    Restriction.LACTOSE,
+    Restriction.ANIMAL_DERIVED,
+}
+
+restrictions_ravioli = {
+    Restriction.LACTOSE,
+    Restriction.GLUTEN,
+}
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("manteiga")
+    ingredient2 = Ingredient("massa de ravioli")
+
+    assert ingredient1.name == "manteiga"
+    assert ingredient2.name == "massa de ravioli"
+
+    assert (ingredient1 == ingredient1) is True
+    assert (ingredient1 == ingredient2) is False
+
+    assert ingredient1.__hash__() == hash("manteiga")
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+
+    assert repr(ingredient1) == "Ingredient('manteiga')"
+    assert repr(ingredient2) != "Ingredient('manteiga')"
+
+    assert ingredient1.restrictions == restrictions_manteiga
+    assert ingredient2.restrictions == restrictions_ravioli
+
+    assert ingredient1 != ingredient2
